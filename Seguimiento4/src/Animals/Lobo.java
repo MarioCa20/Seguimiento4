6a3c5873adb36944e23e3b7e415b243532233965
Mario <mariocanas170@gmail.com>
2022-10-22 01:05:42 -0500

    ejercicio animales

---
+++ Seguimiento4/src/Animals/Lobo.java
@@ -0,0 +1,36 @@
+
+package Animals;
+
+
+public class Lobo extends Canido{
+    /**
+    * Método que devuelve un String con el sonido de un lobo
+    * @return Un valor String con el sonido de un lobo: “Aullido”
+    */
+    public String getSonido() {
+        return "Aullido";
+    }
+    /**
+    * Método que devuelve un String con los alimentos de un lobo
+    * @return Un valor String con el tipo de alimentación de un lobo:
+    * “Carnívoro”
+    */
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
+    /**
+    * Método que devuelve un String con el hábitat de un lobo
+    * @return Un valor String con el hábitat de un lobo: “Bosque”
+    */
+    public String getHábitat() {
+        return "Bosque";
+    }
+    /**
+    * Método que devuelve un String con el nombre científico de un lobo
+    * @return Un valor String con el nombre científico de un lobo:
+    * “Canis lupus”
+    */
+    public String getNombreCientífico() {
+        return "Canis lupus";
+    }
+}
